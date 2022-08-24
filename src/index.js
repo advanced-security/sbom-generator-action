c1948edd1087ce03f74a24f502a7f52a28c9def7
Justin Hutchings <jhutchings1@users.noreply.github.com>
2022-08-23 17:05:49 -0700

    Add persistence to file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const core = require('@actions/core');
 const { Octokit } = require('@octokit/core');
+const { randomUUID } = require('crypto');
+const fs = require('fs');
 const wait = require('./wait');
 require('dotenv').config();
 const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN});
@@ -20,10 +22,28 @@ async function run() {
     core.setFailed(error.message);
   }
 
-  buildSBOM(await getDependencyGraph());
+  let sbom = await buildSBOM(await getDependencyGraph());
+  let fileName = writeFile(sbom, "spdx");
+  core.setOutput("fileName", fileName);
 }
 
+// Writes the given contents to a file and returns the file name. 
+async function writeFile(contents, name) {
+  let filePath = `${process.env.GITHUB_WORKSPACE}/${name}-${randomUUID()}.json`
+  //open a file called filePath and write contents to it
+  fs.writeFile(filePath, contents, function(err) {
+    if(err) {
+      return console.log(err);
+    }
+    core.info("Wrote file to " + filePath);
+  });
+
+  return filePath;
+}
+
+// Builds a SPDX license file from the given dependency graph.
 async function buildSBOM(dependencyGraph) {
+  core.debug("Building SPDX file");
   let spdx = { 
     "spdxVersion": "SPDX-2.3",
     "SPDXID": "SPDXRef-DOCUMENT",
@@ -45,20 +65,23 @@ async function buildSBOM(dependencyGraph) {
     })
   });
 
-  console.log(JSON.stringify(spdx));
+  return JSON.stringify(spdx);
 }
 
+// Returns the PURL for the given dependency.
 function getPurl(dependency) {
   let version = getPackageVersion(dependency.requirements);
   return `pkg:${dependency.packageManager}/${dependency.packageName}@${version}`;
 }
-
+// Returns the package version for the given requirements.
 function getPackageVersion(version) {
   // requirements strings are formatted like '= 1.1.0'
   return version.match('= (.*)')[1];
 }
 
+// Returns the dependency graph for the repository.
 async function getDependencyGraph() {
+  core.debug("Getting repository dependency graph");
   var dependencyGraph = await octokit.graphql(`query($name: String!, $owner: String! ) { 
     repository (owner: $owner, name: $name) {
       dependencyGraphManifests {
--- src/index.test.js
+++ src/index.test.js
@@ -6,18 +6,6 @@ const { default: Ajv } = require('ajv');
 const fs = require('fs');
 const ajv = new Ajv();
 
-test('throws invalid number', async () => {
-  await expect(wait('foo')).rejects.toThrow('milliseconds not a number');
-});
-
-test('wait 500 ms', async () => {
-  const start = new Date();
-  await wait(500);
-  const end = new Date();
-  var delta = Math.abs(end - start);
-  expect(delta).toBeGreaterThanOrEqual(500);
-});
-
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs and validates', async () => {
   process.env['INPUT_MILLISECONDS'] = 100;
