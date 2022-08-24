33c4102fdcffc966bd6190cf2fa607093a259265
Justin Hutchings <jhutchings1@users.noreply.github.com>
2022-08-23 17:07:01 -0700

    Remove template code

--- src/index.js
+++ src/index.js
@@ -9,19 +9,6 @@ const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN});
 
 // most @actions toolkit packages have async methods
 async function run() {
-  try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
-
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
-  }
-
   let sbom = await buildSBOM(await getDependencyGraph());
   let fileName = writeFile(sbom, "spdx");
   core.setOutput("fileName", fileName);
